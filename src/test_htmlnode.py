0b08385a18887d0064e9e2d2f4d0b161c3bc79c8
Kally95 <kally-95@hotmail.com>
2024-03-07 23:55:28 +0000

    seprated tests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,12 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHtmlNode(unittest.TestCase):
+
+    def test_props_to_html(self):
+        node_w_whitespace = HTMLNode(tag='a', value='Click me', props={'href': 'https://www.example.com', 'target': '_blank'})
+
+        self.assertEqual('href="https://www.example.com" target="_blank"', node_w_whitespace.props_to_html()
+        )
+
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -41,13 +41,5 @@ class TestTextNode(unittest.TestCase):
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
 
-class TestHtmlNode(unittest.TestCase):
-    def test_props_to_html(self):
-        node_w_whitespace = HTMLNode(tag='a', value='Click me', props={'href': 'https://www.example.com', 'target': '_blank'})
-
-        self.assertEqual('href="https://www.example.com" target="_blank"', node_w_whitespace.props_to_html()
-        )
-
-
 if __name__ == "__main__":
     unittest.main()
